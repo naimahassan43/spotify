1aad5f4138d9d213269ca53785b6896fb3e8c9b0
Naima Hassan <naima.hassan434@gmail.com>
2021-09-03 11:01:17 +0100

    Playlist rendering

--- js/modules/playlist.js
+++ js/modules/playlist.js
@@ -4,10 +4,36 @@ const Playlist = (() => {
   const songs = songsList;
   const currentPlayIndex = 0;
   const currentSong = new Audio(songs[currentPlayIndex].url);
+  const isPlaying = false;
+
+  const playListElem = document.querySelector(".playlist");
 
   const init = () => {
-    console.log("Hello from playlist");
-    console.log(songs);
+    render();
+  };
+
+  const render = () => {
+    let markup = "";
+
+    songs.forEach((song) => {
+      markup += `
+             <li class="playlist__song">
+              <div class="play-pause">
+                <i class="fa fa-play pp-icon">
+              </i>
+              
+              </div>
+              <div class="playlist__song-details">
+                <span class="playlist__song-name">${song.title}</span>
+                <span class="playlist__song-artist">${song.artist}</span>
+              </div>
+              <div class="playlist__song-duration">${song.time}</div>
+            </li>
+
+     `;
+    });
+
+    playListElem.innerHTML = markup;
   };
   return {
     init,
